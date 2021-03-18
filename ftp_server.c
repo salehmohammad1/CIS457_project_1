302254224285c12d20bccf39c7097694e34a53de
Mohammad Saleh <58119099+salehmohammad1@users.noreply.github.com>
2021-03-18 18:39:52 -0400

    Add files via upload
    ignore for now, redoing the list function

--- ftp_server.c
+++ ftp_server.c
@@ -1,3 +1,8 @@
+
+
+
+
+
 /*********************************************************************
 * Course:       CIS 457 - Project 1
 * Author(s):    Taylor Rieger & Mohammed Saleh
@@ -19,9 +24,11 @@
 #include <strings.h>
 #include <unistd.h>
 #include <sys/stat.h>       //gets file size using stat()
+#include <sys/sendfile.h>   //used for sending a file
 #include <fcntl.h>          //used for file control options
 
-void connectSock(int);
+void connect(int);
+
 void list(int);
 void front(int);
 void sending (int);
@@ -43,7 +50,7 @@ void error(char *msg)
 
 /**********************************************************************
 * Name:     main
-* Author:   Dr. Vijay Bhuse, additions Taylor Rieger and Mohammad Saleh
+* Author:   Dr. Vijay Bhuse, additions Taylor Rieger
 * Date:     March 16, 2021
 * Description: Connects the server to the client and responds to the
                client depending what option they choose.
@@ -55,135 +62,142 @@ int clientcon;
 
 int main(int argc, char *argv[])
 {
-  int sockfd, newsockfd, portno, clilen;
-  char buffer[100];
-  struct sockaddr_in serv_addr, cli_addr;
+     int sockfd, newsockfd, portno, clilen, n, filehandle, size, c, i;
+     char buffer[100], command[5], filename[20];
+     struct sockaddr_in serv_addr, cli_addr;
+     struct stat obj;
 
-  if (argc < 2) {
-      fprintf(stderr,"ERROR, no port provided\n");
-      exit(1);
-  }
+     if (argc < 2) {
+         fprintf(stderr,"ERROR, no port provided\n");
+         exit(1);
+     }
 
-  sockfd = socket(AF_INET, SOCK_STREAM, 0);
-  if (sockfd < 0)
-    error("ERROR opening socket");
+     sockfd = socket(AF_INET, SOCK_STREAM, 0);
+     if (sockfd < 0)
+        error("ERROR opening socket");
 
-  bzero((char *) &serv_addr, sizeof(serv_addr));
-  portno = atoi(argv[1]);
-  serv_addr.sin_family = AF_INET;
-  serv_addr.sin_addr.s_addr = INADDR_ANY;
-  serv_addr.sin_port = htons(portno);
-  if (bind(sockfd, (struct sockaddr *) &serv_addr,
-    sizeof(serv_addr)) < 0)
-      error("ERROR on binding");
+     bzero((char *) &serv_addr, sizeof(serv_addr));
+     portno = atoi(argv[1]);
+     serv_addr.sin_family = AF_INET;
+     serv_addr.sin_addr.s_addr = INADDR_ANY;
+     serv_addr.sin_port = htons(portno);
+     if (bind(sockfd, (struct sockaddr *) &serv_addr,
+              sizeof(serv_addr)) < 0)
+              error("ERROR on binding");
 
-  listen(sockfd,5);
-  i=1;
-	while(1) {
 
-		if(!clientcon)
-    {
-		  newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+
+     listen(sockfd,5);
+
+		i=1;
+	while(i) {
+
+		if(!clientcon)	{
+
+		newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+
+
+
       if (newsockfd < 0)
           error("ERROR on accept");
 		  bzero(buffer,1000);
-    }
 
-	  else 
-    {
-		  clientcon = 1;
-	  }
+	}
+
+	else {
+		clientcon = 1;
+	}
 	
-  }
-
-  pid = fork();
-  if (pid < 0)
-	{
-    error("ERROR on fork");
 	}
+  int pid;
 
-  if (pid == 0)
+   pid = fork();
+        if (pid < 0)
 	{
-    close(sockfd);
-    dostuff(newsockfd);
-    exit(1);
-  }
-  else
-	{
-	  wait (&status);
-	  clientcon = status;
+           error("ERROR on fork");
 	}
+        if (pid == 0)
+	{
+           close(sockfd);
+           dostuff(newsockfd);
+           exit(1);
+        }
+    //    else
+//	{
+    
+	  //wait (&status);
+	   //clientcon = status;
+	//}
 }
 
-void connectSock(int sk ) {
 
-  int n;
-  char buffer[256], command[5];
 
-  bzero(buffer,256);
-  n = read(sk,buffer,255);
-  if (n < 0) error("ERROR reading from socket");
 
-  if(strlen(buffer)<1)
-  {
-	  close(sk);
-	  exit(0);
-  }
-  sscanf(buffer, "%s", command);
-  printf("Command: %s\n", command);
-  printf("BUFFER:%s.\n",buffer);
+void connect(int sk ) 
+{
 
-  //Call the list function if LIST is selected
-  if(strcmp(buffer,"list")==0)
-	  list(sk);
-  
-  //Call the retrieve function if retrieve is selected
-  if (strcmp(buffer,"retrieve")==0)
-  {
-      write(sk, "RETR received",13);
-      fSend(sk);
+ int n;
+   char buffer[256];
+
+   bzero(buffer,256);
+   n = read(sk,buffer,255);
+   if (n < 0) error("ERROR reading from socket");
+
+   if(strlen(buffer)<1)
+   {
+	close(sk);
+	exit(0);
+   }
+   printf("BUFFER:%s.\n",buffer);
+   //Call the list function if LIST is selected
+   if(strcmp(buffer,"LIST")==0)
+   {
+	list(sk);
+   }
+   //Call the retrieve function if retrieve is selected
+   if (strcmp(buffer,"RETR")==0)
+   {
+        write(sk, "RETR received",13);
+        fSend(sk);
+   }
+   //store command:receive a file from client and store in server directory
+   if (strcmp(buffer,"STORE")==0) {
+        write(sk, "STORE received",14); //ack command(fixes consecutive write short message merge)
+        storage(sk); //run function to store file, pass socket number
+   }
+   //Disconnect from the server if QUIT is selected
+   if(strcmp(buffer,"QUIT")==0){
+	close(sk);
+	clientcon=0;
+	exit(0);
+   }
+
   }
-  
-  //store command:receive a file from client and store in server directory
-  if (strcmp(buffer,"store ")==0) {
-      write(sk, "STORE received",14); //ack command(fixes consecutive write short message merge)
-      storage(sk); //run function to store file, pass socket number
-  }
-  
-  //Disconnect from the server if QUIT is selected
-  if(strcmp(buffer,"quit")==0){
-	  close(sk);
-	  clientcon=0;
-	  exit(0);
-  }
-}
+
 
 void list(int sk) {
-
+  int d;
 	char files[1000];
 	int n;
-	DIR *d;
+  int *DIR ; 
+  DIR = d;
 	struct dirent *dir;
+
 	d = opendir(".");
-	if(d)
-  {
-		while((dir = readdir(d)) != NULL)
-    {
-			strcat(files, dir->d_name );
+	if(d){
+		while((dir = readdir(d)) != NULL){
+			//gets(files, dir->dname );
 			strcat(files,"\n");
 		}
 		closedir(d);
 	}
-
-	n= write(sk, strlen(files));
-
-  if (n<0){
+   //strlen(files);
+//	n = write(s);
+if (n<0){
 		error("error establishing scoket ");
-	}
-}
-
+	}}
 //Send file to server
-void sending(int sk)
+void sending  (int sk)
 {
   char cBuff[256];//communication buffer
   FILE *fPoint; //file pointer
@@ -242,7 +256,7 @@ void storage (int sk)
     write(sk, "File length received",20);
     fSize = atol(rBuff);//convert file size from buffer into number
     //write through file until length of file completed.
-    while(count<fSize)
+    while(count < fSize)
     {
       bzero(rBuff,256);//clear receive buffer
       read(sk,rBuff,255);//read data
@@ -252,5 +266,3 @@ void storage (int sk)
     printf("File received.\n");//notify user of completion
     fclose (fPoint);//close file
   }
-}
-
