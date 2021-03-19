e7cef779596136c13ae7eb2f5236d6604893e21b
Mohammad Saleh <58119099+salehmohammad1@users.noreply.github.com>
2021-03-19 04:12:30 -0400

    Add files via upload

--- FTP_server.py
+++ FTP_server.py
@@ -1,11 +1,11 @@
 from pyftpdlib.authorizers import DummyAuthorizer
 from pyftpdlib.handlers import FTPHandler
 from pyftpdlib.servers import FTPServer
-#"C:/Users/Mohammad/FTP/FTPclient.py"
+
 authorizer = DummyAuthorizer()
 authorizer.add_user("user", "00000", "/Users/Mohammad/FTP", perm="elradfmw")
 authorizer.add_anonymous("/Users/Mohammad/FTP")
-#"/Users/JD/Desktop/CIS457/ftp"
+
 handler = FTPHandler
 handler.authorizer = authorizer
 
